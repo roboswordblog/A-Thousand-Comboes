8833ee2e42f28b741004c33445da5cb10407fb16
Robosword <robotanmol@gmail.com>
2026-09-26 00:03:02 -0700

    Refactor Sword and Player class initialization and methods

--- player.py
+++ player.py
@@ -1,54 +1,58 @@
 import pygame
 import settings
 
+
 class Sword:
-  def __init__(self, cooldownBar, player, window):
-    self.x = 0
-    self.y = 0
-    self.cooldownBar = cooldownBar
-    self.dir = 0
-    self.player = player
-    self.actDir = 1
-    self.img = pygame.image.load("assets/sword.png")
-    self.rect = self.image.get_rect()
-    self.rect.x = self.x
-    self.rect.y = self.y
-    self.window = window
-    
-  def update(self):
-    if self.dir == 0:
-      self.actDir = 1
-    else:
-      self.actDir = -1
-      
-    self.dir = self.player.dir
-    self.x = self.player.x + self.actDir * 20
-    self.y = self.player.y
+    def __init__(self, cooldownBar, player, window):
+        self.x = 0
+        self.y = 0
+        self.cooldownBar = cooldownBar
+        self.dir = 0
+        self.player = player
+        self.actDir = 1
+        self.img = pygame.image.load("assets/sword.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.window = window
 
+    def update(self):
+        if self.dir == 0:
+            self.actDir = 1
+        else:
+            self.actDir = -1
 
-  def draw(self):
-    pygame.transform.flip(self.img, self.dir, False)
-    window.blit(self.img, (self.x, self.y))
+        self.dir = self.player.dir
+        self.x = self.player.x + self.actDir * 20
+        self.y = self.player.y
+
+    def draw(self):
+        pygame.transform.flip(self.img, self.dir, False)
+        self.window.blit(self.img, (self.x, self.y))
+
 
 class Player:
-  def __init__ (self, x, y, cooldownBar):
-    self.x = x
-    self.y = y
-    self.camerax = 0
-    self.cameray = 0
-    self.sword = Sword(x, y, cooldownBar, self)
-    self.dir = 0
+    def __init__(self, x, y, cooldownBar, window):
+        self.x = x
+        self.y = y
+        self.camerax = 0
+        self.cameray = 0
+        self.window = window
+        self.sword = Sword(x, y, cooldownBar, self.window)
+        self.dir = 0
+        self.animation = "idle"
+        self.animationFrame = 0
 
-  def update(self):
-    mousex, mousey = pygame.mouse.get_pos()
-    if mousex > settings.midx:
-      self.dir = 0
-      
-    elif mousex < settings.midx:
-      self.dir = 1
-    self.sword.update()
-  
-  def draw(self):
-    pygame.transform.flip(self.img, self.dir, False)
-    window.blit(self.img, (self.x, self.y))
-    self.sword.draw()
+    def update(self):
+        mousex, mousey = pygame.mouse.get_pos()
+        if mousex > settings.midx:
+            self.dir = 0
+
+        elif mousex < settings.midx:
+            self.dir = 1
+        self.sword.update()
+
+    def draw(self):
+        pygame.transform.flip(self.img, self.dir, False)
+        self.window.blit(self.img, (self.x, self.y))
+        self.sword.draw()
